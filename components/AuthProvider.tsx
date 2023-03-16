d670fd140bc64f209de598ac68aadbd09ce1142c
Pablo Valverde Llamas <pablovalverdellamas@gmail.com>
2023-03-16 02:19:48 +0100

    add logrocket

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -3,6 +3,9 @@
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
+import LogRocket from "logrocket";
+LogRocket.init("8tvj77/gigachat-bot");
+
 export default function AuthProvider({ children }: { children: ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
